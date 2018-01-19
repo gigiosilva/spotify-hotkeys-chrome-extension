90e6b567a02afc6b8099d436492a6f1ca5077541
Giovani Silva <giovani.m.silva@gmail.com>
2018-01-19 16:59:40 -0200

    Criado funcao de icone

--- src/background.js
+++ src/background.js
@@ -37,3 +37,12 @@ function onCommand(command) {
 };
 
 chrome.commands.onCommand.addListener(onCommand);
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    // read `newIconPath` from request and read `tab.id` from sender
+    chrome.browserAction.setIcon({
+        path: request.newIconPath,
+        tabId: sender.tab.id
+  });
+});
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,7 @@
 function onInitialize() {
 
-   
+}
+
+function setRepeatIcon() {
+    chrome.runtime.sendMessage({ "newIconPath" : folder + icons[2] });
 }
