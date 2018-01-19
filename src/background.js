bcd41b1f1b2f0253b048f0b006615eb17ed27447
Giovani Silva <giovani.m.silva@gmail.com>
2018-01-19 13:51:10 -0200

    Adicionado repeat

--- src/background.js
+++ src/background.js
@@ -12,11 +12,18 @@ function onCommand(command) {
 
       var code = '';
       if (tab.url.startsWith('https://www.youtube.com')) {
+
+        chrome.tabs.executeScript(tab.id, {
+          file: 'scripts/content.js'
+        });
+
         switch (command) {
           case 'next': code = 'document.querySelector(".ytp-next-button").click()'; break;
-          case 'previous': code = 'document.querySelector(".ytp-prev-button").click()'; break;
+          case 'previous': code = 'window.history.back()'; break;
+          //case 'previous': code = 'document.querySelector(".ytp-prev-button").click()'; break;
           case 'play-pause': code = 'document.querySelector(".ytp-play-button").click()'; break;
           case 'mute-unmute': code = 'document.querySelector(".ytp-mute-button").click()'; break;
+          case 'repeat': code = 'document.querySelector("#ce-repeat").click()'; break;
         }
       }
       if (code.length) {
