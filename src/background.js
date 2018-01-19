57b4472908d857852bf829bca438e95340de7ac0
Giovani Silva <giovani.m.silva@gmail.com>
2018-01-19 17:08:26 -0200

    Adicionado icone repeat

--- src/background.js
+++ src/background.js
@@ -8,7 +8,6 @@ function onCommand(command) {
 
     // Apply command on all youtube tabs.
     for (var tab of tabs) {
-        console.log(tab.url)
 
       var code = '';
       if (tab.url.startsWith('https://www.youtube.com')) {
@@ -23,7 +22,14 @@ function onCommand(command) {
           //case 'previous': code = 'document.querySelector(".ytp-prev-button").click()'; break;
           case 'play-pause': code = 'document.querySelector(".ytp-play-button").click()'; break;
           case 'mute-unmute': code = 'document.querySelector(".ytp-mute-button").click()'; break;
-          case 'repeat': code = 'document.querySelector("#ce-repeat").click()'; break;
+          case 'repeat': {
+            code = 'document.querySelector("#ce-repeat").click()';
+            // chrome.browserAction.setIcon({
+            //   path: request.newIconPath,
+            //   tabId: sender.tab.id
+            // });
+            break;
+          }
         }
       }
       if (code.length) {
